65e279b7838828ba20c5c2f0dc46bfac6c5899f4
hoangnnh <huyhoang.ngoc.nguyen@gmail.com>
2022-03-09 15:09:08 +0700

    Remove cubic-bezier timing function and modify some small code

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
+setInterval(setClock, 1000)
+
 const hourHand = document.querySelector('.hand.hand-hour')
 const minuteHand = document.querySelector('.hand.hand-minute')
 const secondHand = document.querySelector('.hand.hand-second')
 
-setInterval(() => {
-    const currentTime = new Date()
+function setClock() {
+  const currentTime = new Date()
 
-    const secondRatio = currentTime.getSeconds() / 60;
-    const minuteRatio = (currentTime.getMinutes() + secondRatio) / 60;
-    const hourRatio = (currentTime.getHours() + minuteRatio) / 12
+  const secondRatio = currentTime.getSeconds() / 60
+  const minuteRatio = (currentTime.getMinutes() + secondRatio) / 60
+  const hourRatio = (currentTime.getHours() + minuteRatio) / 12
 
-    setRotation(secondHand, secondRatio)
-    setRotation(minuteHand, minuteRatio)
-    setRotation(hourHand, hourRatio)
-
-}, 1000)
+  setRotation(secondHand, secondRatio)
+  setRotation(minuteHand, minuteRatio)
+  setRotation(hourHand, hourRatio)
+}
 
 function setRotation(hand, rotationRatio) {
-    const degree = rotationRatio * 360;
-    hand.style.setProperty('--rotation', degree)
+  const degree = rotationRatio * 360
+  hand.style.setProperty('--rotation', degree)
 }
+
+setClock()
